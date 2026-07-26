0864d6ceb9c9710164064ec851a34f4b5a345cf5
TomasArauz0697 <abdiel06arauz@gmail.com>
2026-07-25 20:40:50 -0500

    just config everything

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css'; // <-- Esta línea es la que inyecta Tailwind
+import './index.css'; // <-- Asegúrate de que esta línea esté presente
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
